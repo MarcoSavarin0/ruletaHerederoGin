d76e8eea5df34d89fd848b19f55ba3779e93868c
MarcoSavarin0 <ateneo2003@gmail.com>
2025-02-10 15:27:30 -0300

    android

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(),
     tailwindcss(),
-    VitePWA({
-        registerType: 'autoUpdate',
-        devOptions: {
-            enabled: true,
-        },
-        manifest: {
-            name: 'Ruleta Festival',
-            short_name: 'Ruleta',
-            description: 'Juego de ruleta para el festival',
-            theme_color: '#000000',
-            background_color: '#ffffff',
-            display: 'standalone',
-            start_url: '/',
-            icons: [
-                {
-                    src: '/icons/icon-192x192.png',
-                    sizes: '192x192',
-                    type: 'image/png'
-                },
-                {
-                    src: '/icons/icon-512x512.png',
-                    sizes: '512x512',
-                    type: 'image/png'
-                }
-            ]
-        },
-        workbox: {
-            globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-        }
-    })
+    // VitePWA({
+    //     registerType: 'autoUpdate',
+    //     devOptions: {
+    //         enabled: true,
+    //     },
+    //     manifest: {
+    //         name: 'Ruleta Festival',
+    //         short_name: 'Ruleta',
+    //         description: 'Juego de ruleta para el festival',
+    //         theme_color: '#000000',
+    //         background_color: '#ffffff',
+    //         display: 'standalone',
+    //         start_url: '/',
+    //         icons: [
+    //             {
+    //                 src: '/icons/icon-192x192.png',
+    //                 sizes: '192x192',
+    //                 type: 'image/png'
+    //             },
+    //             {
+    //                 src: '/icons/icon-512x512.png',
+    //                 sizes: '512x512',
+    //                 type: 'image/png'
+    //             }
+    //         ]
+    //     },
+    //     workbox: {
+    //         globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    //     }
+    // })
     ],
 })
