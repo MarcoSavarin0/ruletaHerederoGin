930f320f5e412503e9677b4250954cd35aaab1d9
MarcoSavarin0 <ateneo2003@gmail.com>
2025-02-10 13:41:03 -0300

    pwa

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'ruleta-herederogin',
-  webDir: 'dist'
-};
-
-export default config;
--- src/utils/wheelUtils.ts
+++ src/utils/wheelUtils.ts
@@ -1,20 +1,29 @@
+import seguiparticipando from "../assets/images/sigueparticipando.png";
+import herederoBotella from "../assets/images/ginHerederoBotella.png"
+import bolsa from "../assets/images/bolsa.png"
+import panuelo from "../assets/images/pañuelo.png"
+import abanico from "../assets/images/abanico.png"
+import cupon20 from "../assets/images/cupon20.png"
+import australiano from "../assets/images/australiano.png"
+
+
 export const prizeOptions = [
-  { option: 'Segui participando', probability: 0.325, image: { uri: "/sigueparticipando.png", sizeMultiplier: 2, offsetY: 70 } },
-  { option: 'Año Gratis', probability: 0.05, quantity: 5, image: { uri: "/ginHerederoBotella.png", sizeMultiplier: 2, offsetY: 70 } },
-  { option: 'Segui participando', probability: 0.325, image: { uri: "/sigueparticipando.png", sizeMultiplier: 2, offsetY: 70  } },
-  { option: 'Totebag', probability: 0.10, quantity: 5, image: { uri: "/bolsa.png", sizeMultiplier: 2, offsetY: 70  } },
-  { option: 'Pañuelo', probability: 0.10, quantity: 5, image: { uri: "/pañuelo.png", sizeMultiplier: 2, offsetY: 70  } },
-  { option: 'Abanico', probability: 0.10, quantity: 5, image: { uri: "/abanico.png", sizeMultiplier: 2, offsetY: 70 } },
-  { option: 'Segui participando', probability: 0.325, image: { uri: "/sigueparticipando.png", sizeMultiplier: 2, offsetY: 70 } },
-  { option: 'Cupon Descuento', probability: 0.10, quantity: 5, image: { uri: "/cupon20.png", sizeMultiplier: 2, offsetY: 70 } },
-  { option: 'Piluso', probability: 0.10, quantity: 5, image: { uri: "/australiano.png", sizeMultiplier: 2, offsetY: 70  } },
+{option: 'Segui participando', probability: 0.325, image: {uri: seguiparticipando, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Año Gratis', probability: 0.05, quantity: 5, image: {uri: herederoBotella, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Segui participando', probability: 0.325, image: {uri: seguiparticipando, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Totebag', probability: 0.10, quantity: 5, image: {uri: bolsa, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Pañuelo', probability: 0.10, quantity: 5, image: {uri: panuelo, sizeMultiplier: 2, offsetY: 70} },
+{option: 'Abanico', probability: 0.10, quantity: 5, image: {uri: abanico, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Segui participando', probability: 0.325, image: {uri: seguiparticipando, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Cupon Descuento', probability: 0.10, quantity: 5, image: {uri: cupon20, sizeMultiplier: 2, offsetY: 70}},
+{option: 'Piluso', probability: 0.10, quantity: 5, image: {uri: australiano, sizeMultiplier: 2, offsetY: 70}},
 ];
 
 export const getRandomPrize = (data: { option: string; probability: number; quantity?: number }[]) => {
-    const random = Math.random();
-    let sum = 0;
-    const validOptions = data.filter(item => item.probability > 0 && (item.quantity === undefined || item.quantity > 0));
-    console.log(validOptions);
+const random = Math.random();
+let sum = 0;
+const validOptions = data.filter(item => item.probability > 0 && (item.quantity === undefined || item.quantity > 0));
+console.log(validOptions);
 
     for (let i = 0; i < validOptions.length; i++) {
         sum += validOptions[i].probability;
